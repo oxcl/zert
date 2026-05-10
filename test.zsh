6a915058f342a1d6199b8b18395b255ed53d45c2
oxcl <oxcl@proton.me>
2026-05-10 15:42:12 +0400

    finish the implementation of plugin loading functionality

--- test.zsh
+++ test.zsh
@@ -2,13 +2,4 @@
 
 source "./zert.zsh"
 
-_zert_ui_task_start "Install Plugin **powerlevel10k**"
-
-_zert_ui_subtask_start 0 "Cloning repository"
-git clone https://github.com/neovim/neovim --filter=tree:0 --progress --verbose /tmp/neovim2 2>&1 | _zert_ui_subtask_log 4
-_zert_ui_subtask_end 0 "ok"
-
-_zert_ui_task_end "ok"
-
-
-sleep 2
+zert romkatv/powerlevel10k
